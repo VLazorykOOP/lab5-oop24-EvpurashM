fca18f390915d40537aa293d1f3456baed2a822e
EvpurasM <yevpurash.maksym@chnu.edu.ua>
2025-04-29 08:54:37 +0300

    code1

--- OOPLab5T/OOPLab5T.cpp
+++ OOPLab5T/OOPLab5T.cpp
@@ -1,24 +1,104 @@
-﻿// OOPLab5T.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+using namespace std;
 
+// Base class
+class Transport {
+public:
+    Transport() {
+        cout << "Transport created.\n";
+    }
 
-#include <iostream>
+    virtual ~Transport() {
+        cout << "Transport destroyed.\n";
+    }
 
-// Ваші файли загловки 
-//
-#include "Lab5Exmaple.h"
-int main()
-{
-    std::cout << " Lab #5  !\n";
-    //  Код виконання завдань
-    //  Головне меню завдань
-    //  Функції та класи можуть знаходитись в інших файлах проекту
+    virtual void printInfo() const {
+        cout << "This is a transport.\n";
+    }
+};
 
-    int chain = 1;
-    if (chain == 1) chain = mainExample1();
-    if (chain == 2) chain = mainExample2();
-    if (chain == 3) chain = mainExample3();
-    if (chain == 4) chain = mainExample4();
-    if (chain == 5) chain = mainExample5();
+// Derived class - Car
+class Car : public Transport {
+public:
+    Car() {
+        cout << "Car created.\n";
+    }
 
+    ~Car() override {
+        cout << "Car destroyed.\n";
+    }
+
+    void printInfo() const override {
+        cout << "This is a car.\n";
+    }
+};
+
+// Derived class - Truck
+class Truck : public Transport {
+public:
+    Truck() {
+        cout << "Truck created.\n";
+    }
+
+    ~Truck() override {
+        cout << "Truck destroyed.\n";
+    }
+
+    void printInfo() const override {
+        cout << "This is a truck.\n";
+    }
+};
+
+// Derived class - Steamship
+class Steamship : public Transport {
+public:
+    Steamship() {
+        cout << "Steamship created.\n";
+    }
+
+    ~Steamship() override {
+        cout << "Steamship destroyed.\n";
+    }
+
+    void printInfo() const override {
+        cout << "This is a steamship.\n";
+    }
+};
+
+// Derived class - Airplane
+class Airplane : public Transport {
+public:
+    Airplane() {
+        cout << "Airplane created.\n";
+    }
+
+    ~Airplane() override {
+        cout << "Airplane destroyed.\n";
+    }
+
+    void printInfo() const override {
+        cout << "This is an airplane.\n";
+    }
+};
+
+// Testing program
+int main() {
+    Transport* vehicles[] = {
+        new Car(),
+        new Truck(),
+        new Steamship(),
+        new Airplane()
+    };
+
+    cout << "\nVehicle information:\n";
+    for (Transport* v : vehicles) {
+        v->printInfo();
+    }
+
+    cout << "\nReleasing memory:\n";
+    for (Transport* v : vehicles) {
+        delete v;
+    }
+
+    return 0;
 }
